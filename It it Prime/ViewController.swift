416a241b3c222a2e24452a49ae810e6389e71553
Brent Mifsud <brent.mifsud@scotiabank.com>
2018-05-10 00:13:34 -0400

    Initial Commit

---
+++ It it Prime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  It it Prime
+//
+//  Created by Mifsud, Brent on 2018-05-10.
+//  Copyright © 2018 Brent Mifsud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ It it PrimeTests/It_it_PrimeTests.swift
@@ -0,0 +1,36 @@
+//
+//  It_it_PrimeTests.swift
+//  It it PrimeTests
+//
+//  Created by Mifsud, Brent on 2018-05-10.
+//  Copyright © 2018 Brent Mifsud. All rights reserved.
+//
+
+import XCTest
+@testable import It_it_Prime
+
+class It_it_PrimeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
