e92a84140dbad36bd1dce399f9df1c700ff72c0f
Brent Mifsud <brent.mifsud@scotiabank.com>
2018-05-10 02:22:40 -0400

    completed app

--- It it Prime/ViewController.swift
+++ It it Prime/ViewController.swift
@@ -15,6 +15,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var textBox: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if let textEntered = textBox.text  {
+            if let numberEntered = Int(textEntered){
+                if isPrime(numberEntered){
+                    resultLabel.text = "It is Prime!"
+                }else{
+                    resultLabel.text = "It is not Prime!"
+                }
+            }else{
+                resultLabel.text = "Please enter a number"
+            }
+        }else{
+            resultLabel.text = "Please enter a value"
+        }
+    }
+    
+    func isPrime(_ number: Int) -> Bool{
+        return number > 1 && !(2..<number).contains{number % $0 == 0}
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
